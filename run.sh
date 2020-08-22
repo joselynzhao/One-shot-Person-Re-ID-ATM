188901f1799ffaf2c4dffdcbb1b7bd5c6fc04298
joselyn <1326549593@qq.com>
2020-08-22 16:07:41 +0800

    增加了args.t参数. 表示tagper采样的倍率.

--- run.sh
+++ run.sh
@@ -1,33 +1,28 @@
-batch_size=16
-dataset=market1501
-epochs=70
-exp_aim=测试atm方法的初步完整效果.
-exp_name=atm
-exp_order=atm_vs_0
-fea=2048
-init=-1.0
-lamda=0.8
-log_name=pl_logs
-loss=ExLoss
-max_frames=100
-momentum=0.5
-resume=yes
-run_file=atm_train.py
-step_size=55
-EF=10
-# train
-python3.6  $run_file --dataset $dataset  --max_frames $max_frames --EF $EF --init $init --loss $loss --fea $fea -m $momentum -e $epochs -s $step_size -b $batch_size --lamda $lamda --exp_order $exp_order --exp_name $exp_name --exp_aim $exp_aim  --log_name $log_name --run_file $run_file
+#batch_size=16
+#dataset=DukeMTMC-VideoReID
+#epochs=70
+#exp_aim=重新测试atm_vs_0的数据
+#exp_name=atm
+#exp_order=atm_0
+#fea=2048
+#init=-1.0
+#lamda=0.8
+#log_name=pl_logs
+#loss=ExLoss
+#max_frames=100
+#momentum=0.5
+#resume=Yes
+#run_file=atm_train.py
+#step_size=55
+#EF=10
+###
+#python3.6  $run_file --dataset $dataset  --max_frames $max_frames --EF $EF --init $init --loss $loss --fea $fea -m $momentum -e $epochs -s $step_size -b $batch_size --lamda $lamda --exp_order $exp_order --exp_name $exp_name --exp_aim $exp_aim  --log_name $log_name --run_file $run_file
 #python3.6  evaluate.py --dataset $dataset  --max_frames $max_frames --EF $EF --init $init --loss $loss --fea $fea -m $momentum -e $epochs -s $step_size -b $batch_size --lamda $lamda --exp_order $exp_order --exp_name $exp_name --exp_aim $exp_aim --log_name $log_name --run_file $run_file
 
 #evalueate
-#dataset=DukeMTMC-VideoReID
-#save_path=/mnt/home/pl_logs/DukeMTMC-VideoReID/atm/atm_vs_0/
-#python3.6 evaluate_simple.py --dataset $dataset  --save_path $save_path
-
-
-
-
-
+dataset=mars
+save_path=/mnt/home/pl_logs/mars/atm/atm_0
+python3.6 evaluate_simple.py --dataset $dataset  --save_path $save_path
 
 
 ##dataset=market1501
